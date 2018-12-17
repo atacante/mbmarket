8a39b5da05528333a2e0477040ecf2de82434e48
Yuri Kotlyar <atacante0666@gmail.com>
2018-12-17 22:36:43 +0200

    final edit_category.php

--- admin/category/edit_category.php
+++ admin/category/edit_category.php
@@ -19,9 +19,8 @@ if (isset($_GET['id'])) {
     $list_all = $db_mbMarket->mysqlListToArray($sql_list_all);
 
 
+    $category_name_1 = $list[0]['category_name'];
 
-
-    $category_name_1 = $list_all['category_name'];
 
     if (!empty($_POST)) {
         $new_category_name = $_POST['category_name'];
@@ -39,53 +38,61 @@ if (isset($_GET['id'])) {
         foreach ($characteristic_arr as $value) {
             $sql_update_char = "INSERT INTO `category_characteristic` (`category_id`, `characteristic_id`) 
                               VALUES ('$edit_id', '$value')";
-            $result_update =  $db_mbMarket->mysqlChange($sql_update_char);
+            $result_update = $db_mbMarket->mysqlChange($sql_update_char);
         }
-        if (!$result||!$result_clear||!$result_update) {
+        if (!$result_update) {
             echo "неверные данные";
         } else {
             header("Location: create_category.php");
         }
     }
 
-}
 
+    ?>
 
-?>
+    <h1>Редактировать:</h1>
+    <form action="edit_category.php?id=<?php echo $edit_id ?>" method="POST">
+        <p>
+            <input type="text" name="category_name" value="<?php echo $category_name_1 ?>"/>
+        </p>
+        <p>Характерстики:<br/>
+            <?php
+            $characteristicSet = false;
+            foreach ($list as $row_set_all) {
+                if (isset($row_set_all['characteristic_id'])) {
+                    $row_select [] = $row_set_all ['characteristic_id'];
+                    $characteristicSet = true;
+                }
+            }
 
-<h1>Редактировать:</h1>
-<form action="edit_category.php?id=<?php echo $edit_id ?>" method="POST">
-    <p>
-        <input type="text" name="category_name" value="<?php echo $category_name_1 ?>"/>
-    </p>
-    <p>Характерстики:<br/>
-        <?php
-        $row_select = [];
-        while ($row_set_arr=mysqli_fetch_assoc($result_select))
-        {
-            $row_select [ ]=$row_set_arr ['characteristic_id'];
-        }
-
-        ?>
-        <select name="characteristic[]" multiple>
-            <!--Show all characteristics-->
-            <?php while ($row_list_all=mysqli_fetch_assoc($result_list_all)){?>
-                <option value="<?php echo $row_list_all['characteristic_id']?>"
-
-                    <?php foreach ($row_select as $selected_characteristic){
-                    if ($selected_characteristic ==$row_list_all['characteristic_id'])
-                    {
-                        echo "selected";
-                        break;
-                    }
-                  
-                }?>
-                ><?php echo $row_list_all['characteristic_name']?></option>
-
-            <?php }
             ?>
-        </select>
-    </p>
-    <p><input type="submit" value="Сохранить"></p>
-</form>
+            <select name="characteristic[]" multiple>
+                <!--Show all characteristics-->
+                <?php foreach ($list_all as $row_list_all) { ?>
+                    <option value="<?php echo $row_list_all['characteristic_id'] ?>"
 
+
+                        <?php
+                        if ($characteristicSet) {
+                            foreach ($row_select as $selected_characteristic) {
+                                if ($selected_characteristic == $row_list_all['characteristic_id']) {
+                                    echo "selected";
+                                    break;
+                                }
+
+                            }
+                        } ?>
+                    ><?php echo $row_list_all['characteristic_name'] ?></option>
+
+                <?php }
+                ?>
+            </select>
+        </p>
+        <p><input type="submit" value="Сохранить"></p>
+    </form>
+    <?php
+} else {
+    echo "нет данных для редактирования <br/>";
+    echo "<a href='create_category.php'>Вернуться к предыдущей странице </a>";
+}
+?>
